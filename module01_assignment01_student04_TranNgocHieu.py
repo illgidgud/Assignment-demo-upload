14539ad7783069594ab304447627ed3040348d4a
NgocHieu0912 <141487268+NgocHieu0912@users.noreply.github.com>
2023-08-12 11:44:21 +0700

    Add files via upload

---
+++ module01_assignment01_student04_TranNgocHieu.py
@@ -0,0 +1,8 @@
+""" Bài tập 1. Python
+a) Ngôn ngữ python được tạo ra bởi Guido van Rossum, một lập trình viên
+người Hà Lan. Phiên bản đầu tiên của python được ra mắt vào ngày
+20/02/1991.
+
+Nguồn tham khảo:
+https://pythoninstitute.org/about-python
+"""
